363011146240948c7547b449549bc3549f28a2f4
黄凯 <527548875@qq.com>
2016-09-09 20:47:04 +0800

    初始化项目

---
+++ HDXSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HDXSwift
+//
+//  Created by huangdaxia on 16/9/9.
+//  Copyright © 2016年 huangdaxia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
