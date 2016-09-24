3181e9b50f5a3b4ffd2a7b06ac9877f7ee848d01
huangdaxia <527548875@qq.com>
2016-09-25 01:40:39 +0800

    AppSetup 设置

--- HDXSwift/Utils/AppSetup.swift
+++ HDXSwift/Utils/AppSetup.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+enum AppTarget {
+    case Test
+    case GM
+    case Production
+    
+    var channelID: String {
+        switch self {
+        case .Test:
+            return ""
+        case .GM:
+            return ""
+        case.Production:
+            return ""
+        }
+    }
+        
+}
+
+class AppSetup {
+    let bundleVersion = (NSBundle.mainBundle().infoDictionary?["CFBundleVersion"] ?? "UnkownVersion") as! String
+    
+    var isDebugging: Bool
+    let target: AppTarget
+    
+    static let shareInstance: AppSetup = AppSetup()
+    
+    private init() {
+        target = .Production
+        isDebugging = true
+    }
+    
+    class func isChinese() -> Bool {
+        if let langs = NSUserDefaults.standardUserDefaults().objectForKey("AppleLanguages") as? [String] {
+            if langs.first?.hasPrefix("zh") == true {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+}
